35a54e15fd7309497b620809a3c23918d8015335
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2022-06-11 12:05:22 +0200

    remove typo

--- src/main/java/at/technikum/calculator/CalculatorController.java
+++ src/main/java/at/technikum/calculator/CalculatorController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class CalculatorController {
 
-    @GetMapping("/sum");
+    @GetMapping("/sum")
     public int sum(@RequestParam int a, @RequestParam int b) {
         return a + b;
     }
