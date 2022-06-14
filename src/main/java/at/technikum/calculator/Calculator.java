6990f4c9d6026fcf1d7cf5b0908a986cb3e2d5fa
wi21b095 <105742571+wi21b095@users.noreply.github.com>
2022-06-14 20:50:25 +0200

    Divide added (#6)
    Divide added

--- src/main/java/at/technikum/calculator/Calculator.java
+++ src/main/java/at/technikum/calculator/Calculator.java
@@ -8,5 +8,13 @@ public class Calculator {
     public int multiply (int a, int b){
         return a * b;
     }
+
+    public int divide (int divisor, int dividend) throws Exception {
+        if (dividend == 0){
+            throw new Exception("Divided by 0");
+        }
+
+        return divisor / dividend;
+    }
 }
 
