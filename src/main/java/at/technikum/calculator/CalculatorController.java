a444ca43a0116186326387b1f8ac592d2da98d0c
wi21b095 <wi21b095@technikum-wien.at>
2022-06-14 20:08:54 +0200

    add tests

--- src/main/java/at/technikum/calculator/CalculatorController.java
+++ src/main/java/at/technikum/calculator/CalculatorController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class CalculatorController {
 
+    Calculator calc = new Calculator();
+
     @GetMapping("/sum")
     public int sum(@RequestParam int a, @RequestParam int b) {
-        return a + b;
+        return calc.sum(a,b);
     }
 }
